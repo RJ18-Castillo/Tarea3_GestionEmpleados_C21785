67560b100c00c7893c49867f6d3597359df935e7
ricardo.castilloacevedo@ucr.ac.cr <ricardo.castilloacevedo@ucr.ac.cr>
2026-05-01 21:56:46 -0600

    Ajuste en interfaz

--- Tarea3_GestionEmpleados_C21785/Repositories/IEmpleadoRepository.cs
+++ Tarea3_GestionEmpleados_C21785/Repositories/IEmpleadoRepository.cs
@@ -5,19 +5,12 @@ namespace Tarea3_GestionEmpleados_C21785.Repositories
     public interface IEmpleadoRepository
     {
         IEnumerable<Empleado> ObtenerTodos();
-
         Empleado? ObtenerPorId(int id);
-
         IEnumerable<Empleado> BuscarPorNombreODepartamento(string termino);
-
         IEnumerable<Empleado> ObtenerPaginado(int pagina, int tamano, string? busqueda);
-
         int ContarTotal(string? busqueda);
-
         void Agregar(Empleado empleado);
-
         void Actualizar(Empleado empleado);
-
         void Eliminar(int id);
     }
 }
