3e6e4c7507eba38c8a365833db4975b118475ea1
ricardo.castilloacevedo@ucr.ac.cr <ricardo.castilloacevedo@ucr.ac.cr>
2026-05-01 21:02:41 -0600

    Agregar archivos de proyecto.

---
+++ Tarea3_GestionEmpleados_C21785/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tarea3_GestionEmpleados_C21785.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
