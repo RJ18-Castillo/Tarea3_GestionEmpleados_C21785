ab3f4eebaf8ea2f76104991e7f7bf65891bb3f45
ricardo.castilloacevedo@ucr.ac.cr <ricardo.castilloacevedo@ucr.ac.cr>
2026-05-01 21:55:30 -0600

    Ajuste en model empleado

--- Tarea3_GestionEmpleados_C21785/Models/Empleado.cs
+++ Tarea3_GestionEmpleados_C21785/Models/Empleado.cs
@@ -18,14 +18,13 @@ namespace Tarea3_GestionEmpleados_C21785.Models
         public string Departamento { get; set; }
 
         [Required(ErrorMessage = "El salario es obligatorio")]
-        [Range(400000, 10000000, ErrorMessage = "El salario debe estar entre 400000 y 10000000")]
+        [Range(400000, 10000000)]
         public decimal Salario { get; set; }
 
         public DateTime FechaIngreso { get; set; }
 
         public bool Activo { get; set; }
 
-        // 🔥 PROPIEDAD CALCULADA (te la revisan fijo)
         public string NombreCompleto => $"{Nombre} {Apellidos}";
     }
 }
