eed36f5ebeda4d3e667b8c59ff4b64262f1689c2
ricardo.castilloacevedo@ucr.ac.cr <ricardo.castilloacevedo@ucr.ac.cr>
2026-05-01 21:37:24 -0600

    Creación del contenido de AppDbContext e instalación del paquete de MS.EntityFrameworkCore.

--- Tarea3_GestionEmpleados_C21785/Data/AppDbContext.cs
+++ Tarea3_GestionEmpleados_C21785/Data/AppDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace Tarea3_GestionEmpleados_C21785.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Tarea3_GestionEmpleados_C21785.Models;
+
+namespace Tarea3_GestionEmpleados_C21785.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Empleado> Empleados { get; set; }
     }
 }
